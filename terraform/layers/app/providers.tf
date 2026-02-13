baa5fd75b2b61b4ddb5517260f4981fec0a73144
Charlotte Wilkinson <c.wilkinson@Charlotte-Wilkinsons-MacBook-Pro-Reply.local>
2026-02-13 11:18:23 +0000

    Ticket 2 Configure default tags AGAIN

--- terraform/layers/app/providers.tf
+++ terraform/layers/app/providers.tf
@@ -7,3 +7,16 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      Environment = "sandbox19"
+      Project     = "lab1"
+      Owner       = "charlotte"
+      ManagedBy   = "terraform"
+    }
+  }
+}
--- terraform/layers/base/providers.tf
+++ terraform/layers/base/providers.tf
@@ -1,9 +1,22 @@
 terraform {
-  required_version = "~> 1.5.5"
+  required_version = "~> 1.14.4"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      Environment = "sandbox19"
+      Project     = "lab1"
+      Owner       = "charlotte"
+      ManagedBy   = "terraform"
     }
   }
 }
