1445d9c4ba2e158ca1e58b9120ce5e41d0d9f1a8
Charlotte Wilkinson <c.wilkinson@Charlotte-Wilkinsons-MacBook-Pro-Reply.local>
2026-02-16 17:02:33 +0000

    Updated with PR comments.

--- terraform/layers/base/vpc.tf
+++ terraform/layers/base/vpc.tf
@@ -9,21 +9,16 @@ locals {
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
 
-    name = "my-vpc"
+    name = "${var.project_name}-${var.environment}-vpc"
     cidr = var.cidr
 
 
     azs = local.azs
-    private_subnets = [for k, v in local.azs : cidrsubnet("10.0.0.0/16", 3, k)]
+    private_subnets = [for k, v in local.azs : cidrsubnet(var.cidr, 3, k)]
     # separate from private subnet 
-    public_subnets = [for k, v in local.azs : cidrsubnet("10.0.0.0/16", 3, k + 4)]
+    public_subnets = [for k, v in local.azs : cidrsubnet(var.cidr, 3, k + 4)]
   
     enable_nat_gateway = true
     single_nat_gateway = var.environment == "dev" ? true : false
     one_nat_gateway_per_az = var.environment == "dev" ? false : true
-
-    tags = {
-    Terraform = "true"
-    Environment = var.environment
-    }
 }
