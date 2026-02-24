949f950f03e503cf13f2544448b656247fa3128a
Charlotte Wilkinson <c.wilkinson@REYUK-T25083P7.replynet.prv>
2026-02-24 14:15:59 +0000

    Configure health checks and listener.

--- terraform/layers/app/alb.tf
+++ terraform/layers/app/alb.tf
@@ -3,7 +3,7 @@ module "alb" {
 
   name = "${var.project_name}-${var.environment}-alb"
   vpc_id  = data.aws_vpc.vpc.id
-  subnets = data.aws_subnets.public.id
+  subnets = data.aws_subnets.public.ids
 
   # Security Group
   security_group_ingress_rules = {
@@ -29,30 +29,15 @@ module "alb" {
     }
   }
 
-  # access_logs = {
-  #   bucket = "my-alb-logs"
-  # }
-
-  # listeners = {
-  #   ex-http-https-redirect = {
-  #     port     = 80
-  #     protocol = "HTTP"
-  #     redirect = {
-  #       port        = "443"
-  #       protocol    = "HTTPS"
-  #       status_code = "HTTP_301"
-  #     }
-  #   }
-  #   ex-https = {
-  #     port            = 443
-  #     protocol        = "HTTPS"
-  #     certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-  #     forward = {
-  #       target_group_key = "ex-instance"
-  #     }
-  #   }
-  # }
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
 
   target_groups = {
     ex-instance = {
@@ -72,9 +57,4 @@ module "alb" {
       }
     }
   }
-
-  # tags = {
-  #   Environment = "Development"
-  #   Project     = "Example"
-  # }
 }
